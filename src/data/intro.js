0b208e8d02fe2c8dc832439de03a5cb1ca94ba6c
godcandidate <102991532+godcandidate@users.noreply.github.com>
2025-05-23 16:16:13 +0000

    Update intro.js

--- src/data/intro.js
+++ src/data/intro.js
@@ -3,29 +3,34 @@ const testsData = [
     id: 0,
     title: "AWS SAA-C03",
     description:
-      "Welcome to the AWS Solution Architect Associate Exams, practice and test yourself",
-    numberOfTests: 4,
+      "Welcome to the AWS Solution Architect Associate Exams, practice and test yourself\n Feel free to cross check answers if you doubt",
+    numberOfTests: 5,
   },
   {
     id: 1,
-    title: "Test 1",
+    title: "CodeSignal 1 - A",
     numberOfTests: 30,
   },
   {
     id: 2,
-    title: "Test 2",
+    title: "CodeSignal 1 - B",
     numberOfTests: 30,
-  },
-  {
-    id: 3,
-    title: "Test 3",
-    numberOfTests: 5,
   },
     {
     id: 4,
     title: "Test 4 - Dumps",
     numberOfTests: 32,
   },
+      {
+    id: 5,
+    title: "CodeSignal 2 - A",
+    numberOfTests: 32,
+  },
+      {
+    id: 6,
+    title: "CodeSignal 2 - B",
+    numberOfTests: 32,
+  }
 ];
 
 export default testsData;
